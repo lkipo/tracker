958ad028f204a247dd086ba7e9e6ae3db548c059
lkipo <loisnodar@gmail.com>
2022-05-04 12:41:18 +0200

    4Ma

--- camera_calibration/test_camera_calibration_opencv.py
+++ camera_calibration/test_camera_calibration_opencv.py
@@ -3,13 +3,15 @@ import cv2 as cv
 import glob
 
 # image name
-imname = 'descarga.jpeg'
+imname = 'calib_radial.jpg'
+square_size = 10 # cm
+
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp[:,:2] = square_size * np.mgrid[0:7,0:6].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -20,6 +22,7 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 # Find the chess board corners
 ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+print(corners)
 
 # If found, add object points, image points (after refining them)
 if ret == True:
