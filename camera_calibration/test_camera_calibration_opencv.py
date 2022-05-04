9175c3de9f1470323c91795b42ec3f04abee7deb
lkipo <loisnodar@gmail.com>
2022-05-04 20:37:22 +0200

    quitado codigo comentado. en proceso de unir parte b non funciona a parte final.

--- camera_calibration/test_camera_calibration_opencv.py
+++ camera_calibration/test_camera_calibration_opencv.py
@@ -2,6 +2,12 @@ import numpy as np
 import cv2 as cv
 import glob
 
+    def draw(img, corners, imgpts):
+        corner = tuple(corners[0].ravel())
+        img = cv.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
+        img = cv.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
+        img = cv.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
+
 # image name
 imname = 'calib_radial.jpg'
 square_size = 10 # cm
@@ -40,13 +46,6 @@ if ret == True:
     img = cv.imread(imname)
     h,  w = img.shape[:2]
     newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-
-    '''# undistort
-    dst = cv.undistort(img, mtx, dist, None, newcameramtx)
-    
-    # crop the image
-    x, y, w, h = roi
-    dst = dst[y:y+h, x:x+w]'''
     
     # undistort
     mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
@@ -61,6 +60,23 @@ if ret == True:
     cv.waitKey()
 
     print(newcameramtx)
-    np.savez(file='B',  dst=dst, dist=dist, rvecs=rvecs, tvecs=tvecs)
+
+    # Estaría ben saber usar esto -> np.savez(file='B',  dst='dst', dist='dist', rvecs='rvecs', tvecs='tvecs')
+ 
+    objp = np.zeros((6*7,3), np.float32)
+    objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+    axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
+
+    corners2 = cv.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+    # Find the rotation and translation vectors.
+    ret,rvecs, tvecs = cv.solvePnP(objp, corners2, mtx, dist)
+    # project 3D points to image plane
+    imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, mtx, dist)
+    img = draw(img,corners2,imgpts)
+    cv.imshow('img',img)
+    k = cv.waitKey(0) & 0xFF
+    if k == ord('s'):
+        cv.imwrite(imname[:6]+'.png', img)
+
 
 cv.destroyAllWindows()
